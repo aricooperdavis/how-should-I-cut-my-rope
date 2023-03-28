b4f5a1ff601235fa95c211eae1783952d942c1b0
Ari Cooper-Davis <ari@cooper-davis.net>
2023-03-28 12:52:48 +0100

    First working alpha

---
+++ readme.md
@@ -0,0 +1,10 @@
+# What could I rig?
+Enter the ropes you want to cut, and the ropes you already have, to find out the trips you could rig by cutting your rope into different lengths.
+
+Find it at: [aricooperdavis.github.io/what-could-I-rig](https://aricooperdavis.github.io/what-could-I-rig/)
+
+### Data Sources
+This tool is driven by the same data that drives the [what can I rig](https://github.com/aricooperdavis/what-can-I-rig/) tool.
+
+### Corrections
+Please feel free to [raise an issue](https://github.com/aricooperdavis/what-could-I-rig/issues) if you spot mistakes, or if you're familiar with Git then make the changes yourself and open a pull request.
---
+++ script.js
@@ -0,0 +1,166 @@
+// Trip files
+let regions = ['derbyshire','scotland','yorkshire'];
+
+// Remove rope input
+function removeRope(event) {
+  event.target.parentNode.remove();
+}
+
+// Add rope input
+function addRope(el) {
+  let newInput = ropeInput.cloneNode(true);
+  newInput.lastChild.disabled = false;
+  newInput.firstChild.value = '';
+  newInput.firstChild.onkeydown = handleTabs;
+  addButton.parentNode.insertBefore(newInput, el.target ?? el);
+  for (let el of document.getElementsByClassName('rope')) {
+    el.lastChild.onclick = removeRope;
+  };
+  return newInput;
+};
+let addButton = document.getElementById('add');
+addButton.onclick = addRope;
+
+function handleTabs (event) {
+  if (event.code == 'Tab') {
+    let newInput = addRope(event.target.parentNode.nextElementSibling);
+    event.preventDefault();
+    newInput.firstChild.focus();
+
+  }
+}
+let ropeInput = document.getElementsByClassName('rope')[0];
+ropeInput.onkeydown = handleTabs;
+
+// Handle reset button
+let tableData = document.getElementById('results-data');
+let resetButton = document.getElementById('reset');
+resetButton.onclick = function reset() {
+  // remove all rope inputs
+  for (let el of document.querySelectorAll('.rope')) {
+    if (!el.lastChild.disabled) {
+      el.remove();
+    } else {
+      el.firstChild.value = '';
+    };
+  };
+  // remove results data
+  tableData.innerHTML = '';
+  // Uncheck cut
+  Array.from(document.getElementsByClassName('cut')).forEach(el => el.checked = false);
+};
+
+// Handle go button
+let results_table = document.getElementById('results-table');
+let goButton = document.getElementById('go');
+goButton.onclick = function calculate() {
+
+  // Get user rope details
+  let resn = document.getElementById('resolution').value;
+  let set_ropes = [];
+  let cuttable_ropes = [];
+  for (let el of document.getElementsByClassName('rope')) {
+    let rope = parseInt(el.firstChild.value);
+    if (rope) {
+      if (el.children[1].lastChild.checked) {
+        cuttable_ropes.push(rope);
+      } else {
+        set_ropes.push(rope);
+      }
+    }
+  }
+  set_ropes.sort((a,b) => a < b);
+
+  // Reset output
+  tableData.innerHTML = '';
+
+  // Get trip lengths
+  Promise.all(
+    regions.map(region => 
+    fetch(`https://raw.githubusercontent.com/aricooperdavis/what-can-I-rig/main/pitchlengths/${region}.txt`)
+      .then(response => response.text())
+  )).then((result) => {
+
+    // Parse and sort pitch lengths
+    trips = result.join('').split('\n').filter(
+      line => !(line.startsWith('#') | line.length < 3)
+    ).map(
+      trip => (
+        trip.includes('[') ? trip.split(',').slice(3,) : trip.split(',').slice(1,)
+      ).map(
+        pitch => parseInt(pitch)
+      ).sort((a,b) => a < b)
+    ).sort((a,b) => (a.length > b.length));
+    
+    // Determine all cut options
+    all_cuts = []
+    cuttable_ropes.forEach(cr => {
+      let _cuts = combos(Math.floor(cr/resn)).map(c => c.map(r => r*5));
+      all_cuts.push(_cuts);
+    });
+
+    // Calculate possible trips for all combinations
+    if (all_cuts.length == 0) {
+      let _count = possibleTripsCount(trips, set_ropes);
+      let row = `<tr><td>${0}</td><td>${JSON.stringify(set_ropes)}</td><td>${_count}</td></tr>`;
+      tableData.insertAdjacentHTML('beforeend', row);
+    } else {
+      let opts = [...cartesian(...all_cuts)];
+      opts.forEach(opt => {
+        let _opt = [...set_ropes, ...opt].flat().sort().reverse();
+        let _cuts = opt.map(x=>x.length).reduce((a,b)=>a+b,0)-1;
+        let _count = possibleTripsCount(trips, _opt);
+        let row = `<tr><td>${_cuts}</td><td>${JSON.stringify(opt)}</td><td>${_count}</td></tr>`;
+        tableData.insertAdjacentHTML('beforeend', row);
+      });
+    }
+  });
+  sorttable.makeSortable(results_table);
+};
+
+// N.b. trips is array (sorted by increasing rope-count) of arrays (sorted by decreasing rope-length)
+// ropes is sorted by decreasing rope-length
+function possibleTripsCount(trips, ropes) {
+  let count = 0;
+  for (let t = 0; t < trips.length; t++) {
+    let trip = trips[t];
+    // Short circuit where possible
+    if (trip.length > ropes.length) break;
+    if (trip[0] > ropes[0]) continue;
+    // Count possible trips
+    if (trip.every((pitch, i) => pitch <= ropes[i])) count++;
+  }
+  return count;
+}
+
+// Get cartesian product of multiple arrays (select 1 element from each array)
+function* cartesian(head, ...tail) {
+  const remainder = tail.length > 0 ? cartesian(...tail) : [[]];
+  for (let r of remainder) for (let h of head) yield [h, ...r];
+}
+
+// Get all possible combinations of positive integers that sum to `n`
+function combos(n) {
+  const cache = [[], [[1]], [[2], [1, 1]]];
+  return [...(new Set([...combo(n).map(v => v.join(','))]))].map(v=>v.split(',').map(w=>parseInt(w)));
+
+  function combo(n) {
+      var a = n - 1, b, insert;
+      if (cache[n]) { return cache[n] }
+
+      const res = n % 2 ? [[n]] : [[n], [n >> 1, n >> 1]];
+      while (a > n - a) {
+          b = n - a;
+          for (const sub of combo(a--)) {
+              const subRes = [];
+              insert = true;
+              for (const v of sub) {
+                  v > b || !insert ? subRes.push(v) : (insert = false, subRes.push(b, v)); 
+              }
+              insert && subRes.push(b);
+              res.push(subRes);
+          }
+      }
+      return cache[n] = res;
+  }
+}
